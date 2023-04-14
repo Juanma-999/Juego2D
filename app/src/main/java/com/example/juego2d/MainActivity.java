87d97d277dca063e3c9cea311dd15363519c0a58
Juanma-999 <jmvillaverde99@hotmail.com>
2023-04-14 18:33:25 +0200

    Game loop

--- app/src/main/java/com/example/juego2d/MainActivity.java
+++ app/src/main/java/com/example/juego2d/MainActivity.java
@@ -1,14 +1,27 @@
 package com.example.juego2d;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Game game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // Configurar pantalla completa
+        Window window = getWindow();
+        window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
+        // Hacer visibles los objetos de la clase Game
+        game = new Game(this);
+        setContentView(game);
+
     }
 }
